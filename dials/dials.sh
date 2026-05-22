0f4433d28f619c13fc0fd6d9b71e7e4764379b6c
Samuel-Foster62 <135386877+Samuel-Foster62@users.noreply.github.com>
2026-05-22 09:49:12 +0100

    Add dials.sh script for processing XRD images from VMXm

---
+++ dials/dials.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+# Define the source and destination directories
+images=`ls -1v /CHANGE/THIS/PATH/*.nxs`
+src_dir=$(pwd)
+for wedge in $images
+do
+  echo $wedge
+  name=`basename $wedge | cut -d '.' -f 1`
+  date=`echo $wedge | cut -d '/' -f 7`
+  echo $date
+  directory=$src_dir/CHANGE/THIS/PATH/$name
+  if [ -d $directory ]
+  then
+    echo "$directory exists: skipping"
+    continue
+  fi
+  mkdir -p $directory
+  cd $directory
+
+  # Create the 'run.sh' file inside the new directory
+  cat > run.sh << EOF
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:30:00
+#SBATCH --error=j_%J.err
+#SBATCH --output=j_%J.out
+#SBATCH -p cs04r
+
+module load dials/latest
+
+#xia2 image=$wedge \
+#  remove_blanks=True \
+#  failover=True \
+#  ice_rings.unit_cell=3.615,3.615,3.615,90,90,90 \
+#  ice_rings.space_group=fm-3m \
+#  ice_rings.width=0.01 \
+#  ice_rings.filter=True \
+#  find_spots.phil_file=/dls/science/groups/i02-1/scripts/processing/spots.phil
+dials.import $wedge geometry.detector.distance=313.5
+dials.find_spots /dls/science/groups/i02-1/scripts/processing/spots.phil imported.expt d_min=2.2 d_max=20.0
+dials.index strong.refl imported.expt
+dials.refine indexed.* scan_varying=True
+dials.integrate refined.*
+
+EOF
+
+chmod +x $directory/run.sh
+  # Submit the job to Slurm
+  cd $directory
+
+  sbatch "run.sh"
+
+  echo "Submitted Slurm job for $dataset_name"
+
+done
+
